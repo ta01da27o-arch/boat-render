b54cafd3ab1c0dc281dc2bb8d093d99db7e9f357
ta01da27o-arch <ta01da27o@gmail.com>
2026-03-16 07:46:35 +0900

    Create fetchRaces.js

---
+++ services/fetchRaces.js
@@ -0,0 +1,32 @@
+export async function fetchRaces(){
+
+return [
+
+{jcd:"01",name:"桐生"},
+{jcd:"02",name:"戸田"},
+{jcd:"03",name:"江戸川"},
+{jcd:"04",name:"平和島"},
+{jcd:"05",name:"多摩川"},
+{jcd:"06",name:"浜名湖"},
+{jcd:"07",name:"蒲郡"},
+{jcd:"08",name:"常滑"},
+{jcd:"09",name:"津"},
+{jcd:"10",name:"三国"},
+{jcd:"11",name:"びわこ"},
+{jcd:"12",name:"住之江"},
+{jcd:"13",name:"尼崎"},
+{jcd:"14",name:"鳴門"},
+{jcd:"15",name:"丸亀"},
+{jcd:"16",name:"児島"},
+{jcd:"17",name:"宮島"},
+{jcd:"18",name:"徳山"},
+{jcd:"19",name:"下関"},
+{jcd:"20",name:"若松"},
+{jcd:"21",name:"芦屋"},
+{jcd:"22",name:"福岡"},
+{jcd:"23",name:"唐津"},
+{jcd:"24",name:"大村"}
+
+]
+
+  }
